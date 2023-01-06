17261b7c366218a5c1ee84e611708abef666bf75
Alberto Brealey-Guzman <alberto@feltfragments.com>
2023-01-06 17:25:48 +0000

    backend: Add auth and session management

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,4 +1,6 @@
+import { createAuth } from '@keystone-next/auth';
 import { config, createSchema } from '@keystone-next/keystone/schema';
+import { statelessSessions, withItemData } from '@keystone-next/keystone/session';
 import 'dotenv/config';
 import { User } from './schemas/User';
 
@@ -9,21 +11,35 @@ const sessionConfig = {
   secret: process.env.COOKIE_SECRET,
 };
 
-export default config({
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true,
-    },
-  },
-  db: {
-    adapter: 'mongoose',
-    url: databaseURL,
-  },
-  lists: createSchema({
-    User,
-  }),
-  ui: {
-    isAccessAllowed: () => true,
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
   },
 });
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+    },
+    lists: createSchema({
+      User,
+    }),
+    ui: {
+      isAccessAllowed: ({ session }) => !!session?.data,
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: 'id',
+    }),
+  })
+);
