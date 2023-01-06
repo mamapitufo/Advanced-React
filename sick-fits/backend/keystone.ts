48b7d16c1c146af7253df429512efc90a6651009
Alberto Brealey-Guzman <alberto@feltfragments.com>
2023-01-06 16:21:15 +0000

    Add skeleton Keystone config
    Bootstraps the system, but there is no schema or users.

---
+++ sick-fits/backend/keystone.ts
@@ -0,0 +1,26 @@
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import 'dotenv/config';
+
+const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 30,
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+  },
+  lists: createSchema({}),
+  ui: {
+    isAccessAllowed: () => true,
+  },
+});
