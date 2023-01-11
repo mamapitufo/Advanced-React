c62597357a233d7a3895d69bb024c3985f86ee14
Alberto Brealey-Guzman <alberto@feltfragments.com>
2023-01-11 16:56:04 +0000

    fe: Add Apollo client provider

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,16 +1,31 @@
 import { Router } from 'next/router';
 import nProgress from 'nprogress';
 import '../components/styles/nprogress.css';
+import withApollo from '../lib/withData';
+import { ApolloProvider } from '@apollo/client';
 import Page from '../components/Page';
 
 Router.events.on('routeChangeStart', () => nProgress.start());
 Router.events.on('routeChangeComplete', () => nProgress.done());
 Router.events.on('routeChangeError', () => nProgress.done());
 
-export default function App({ Component, pageProps }) {
+function App({ Component, pageProps, apollo }) {
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
+
+App.getInitialProps = async function getInitialProps({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+export default withApollo(App);
