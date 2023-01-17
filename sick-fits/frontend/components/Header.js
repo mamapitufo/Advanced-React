64d0e9b4c830dab8c803b1bd68ea5d1dfbe2be04
Alberto Brealey-Guzman <alberto@feltfragments.com>
2023-01-17 18:50:32 +0000

    fe: Style main navigation

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -23,7 +23,7 @@ const HeaderStyled = styled.header`
     display: grid;
     grid-template-columns: auto 1fr;
     justify-content: space-between;
-    align-items: center;
+    align-items: stretch;
   }
 
   .sub-bar {
@@ -40,9 +40,9 @@ export default function Header() {
         <Logo>
           <Link href="/">Sick Fits</Link>
         </Logo>
+        <Nav />
       </div>
       <div className="sub-bar">Search</div>
-      <Nav />
     </HeaderStyled>
   );
 }
