60dabd624803662b1c995d6f44d5f54cc556292d
Alberto Brealey-Guzman <alberto@feltfragments.com>
2023-01-18 12:12:41 +0000

    fe: Add mutation to create a new product
    Handles image uploads, loading statea and errors.

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,22 +1,46 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
 import useForm from '../lib/useForm';
+import DisplayError from './ErrorMessage';
 import Form from './styles/Form';
 
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION($name: String!, $price: Int!, $description: String!, $image: Upload) {
+    createProduct(
+      data: {
+        name: $name
+        price: $price
+        description: $description
+        photo: { create: { image: $image, altText: $name } }
+        status: "AVAILABLE"
+      }
+    ) {
+      id
+      name
+    }
+  }
+`;
+
 export default function CreateProduct() {
-  const { inputs, setInputValue, resetForm, clearForm } = useForm({
+  const { inputs, setInputValue, clearForm } = useForm({
     image: '',
     name: '',
     price: 0,
     description: '',
   });
 
+  const [createProduct, { loading, error, data }] = useMutation(CREATE_PRODUCT_MUTATION, { variables: inputs });
+
   return (
     <Form
-      onSubmit={(e) => {
+      onSubmit={async (e) => {
         e.preventDefault();
-        console.log(inputs);
+        await createProduct();
+        clearForm();
       }}
     >
-      <fieldset>
+      <DisplayError error={error} />
+      <fieldset disabled={loading} aria-busy={loading}>
         <label htmlFor="image">
           <input type="file" id="image" name="image" onChange={setInputValue} required />
         </label>
